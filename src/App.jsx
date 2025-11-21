a52a858e5dba0fed616d5bcc93bd69fd9f45b8b2
unknown <abdielmamani@upeu.edu.pe>
2025-11-21 13:07:22 -0500

    sidebar 20%

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,80 @@
 import { useState } from 'react'
 
-import './App.css'
 import {AuthContextProvider} from "./index";
 import {MyRoutes} from "./routers/routes"
+import styled, { ThemeProvider } from 'styled-components';
+import { createContext } from 'react';
+import { Device } from './styles/breakpoints';
+import { Light, Dark } from './index';
 
+
+export const ThemeContext = createContext(null);
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeuse, setTheme] = useState("dark");
+  const theme= themeuse==="light"?"light":"dark"
+  const themeStyle = theme==="light"?Light:Dark;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <>
-      <AuthContextProvider>
-        <MyRoutes></MyRoutes>
-      </AuthContextProvider>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <ThemeProvider theme={themeStyle}>
+        <AuthContextProvider>
+          <Container className={sidebarOpen?"active":""}>
+            <section className="ContentSidebar">
+              Sidebard
+            </section>
+            <section className="ContentMenuambur">
+              Menu Ha
+            </section>
+            <section className='ContentRoutes'>
+                <MyRoutes></MyRoutes>
+            </section>
+
+            
+          </Container>
+        </AuthContextProvider>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+      
     </>
 
   )
 }
 
+const Container  = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color:${({theme})=>theme.bgtotal};
+
+  .ContentSidebar{
+    display:none;
+  }
+  .ContentMenuambur{
+    display:block;
+    position:absolute;
+    left:20px;
+  }
+
+  @media ${Device.tablet}{
+    grid-template-columns: 65px 1fr;
+    &.active{
+      grid-template-columns: 220px 1fr;
+    }
+    .ContentSidebar{
+      display:initial;
+    }
+    .ContentMenuambur{
+      display:none;
+    }
+  }
+  .ContentRoutes{
+    grid-column: 1;
+    width:100%;
+    @media ${Device.tablet}{
+      grid-column: 2;
+    }
+  }
+
+`;
 export default App
--- src/components/templates/HomeTemplate.jsx
+++ src/components/templates/HomeTemplate.jsx
@@ -4,4 +4,15 @@ export function HomeTemplate(){
         <h1>Home Template</h1>
     </Container>)
 }
-const Container = styled.div` `
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    overflow: hidden;
+    background-color: ${({theme})=>theme.bgtotal};
+    color: ${({theme})=>theme.text};
+    width: 100%;
+
+
+`
